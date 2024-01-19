fd4756a15edf72be42b5413ebb68066f8b7f3074
Avery Gan <agan@c2r5s14.42singapore.sg>
2024-01-19 14:01:27 +0800

    updated main for lexer tests

--- srcs/init/main_avery.c
+++ srcs/init/main_avery.c
@@ -33,18 +33,36 @@
 // 	return (0);
 // }
 
-// Testing main linked list functions
-int main(void)
-{
-	t_token *node;
-	t_token *token_list;
-	token_list = NULL;
-	char *s1 = "first string";
-	char *s2 = "second string";
+// // Testing main linked list functions
+// int main(void)
+// {
+// 	t_token *node;
+// 	t_token *token_list;
+// 	token_list = NULL;
+// 	char *s1 = "first string";
+// 	char *s2 = "second string";
 
-	node = create_node(s1);
-	token_add_back(&token_list, node);
-	node = create_node (s2);
-	token_add_back(&token_list, node);
-	print_token_list(token_list);
+// 	node = create_node(s1);
+// 	token_add_back(&token_list, node);
+// 	node = create_node (s2);
+// 	token_add_back(&token_list, node);
+// 	print_token_list(token_list);
+// }
+
+// Lexer main
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	if (argc > 1 && argv)
+	{
+		ft_putstr_fd("Program does not accept arguments.\n", 2);
+		return (0);
+	}
+	array_dup(&data, envp);
+	//testing
+	data.history_list = malloc(sizeof(t_history));
+	data.history_list->input = readline("Minishell:~$ ");
+	ft_lexer(data.history_list->input);
+	return (0);
 }
