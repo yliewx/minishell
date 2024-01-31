86a710c7b930efbb1e6c5598bf02803b8700570f
Yun Xuan Liew <yliew@c2r1s9.42singapore.sg>
2024-01-31 17:12:11 +0800

    added main

--- srcs/init/main.c
+++ srcs/init/main.c
@@ -11,59 +11,35 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-/*
-void	make_test_nodes(t_node **node)
-{
-	(*node) = malloc(sizeof(t_node));
-	(*node)->type = T_AND;
-	(*node)->value = "&&";
-	(*node)->left = malloc(sizeof(t_node));
-	(*node)->left->type = T_STRING;
-	(*node)->left->value = "export test=123";
-	(*node)->right = malloc(sizeof(t_node));
-	(*node)->right->type = T_STRING;
-	(*node)->right->value = "env";
-}
-// */
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	t_minishell	minishell;
-// 	//char	*command;
 
-// 	if (argc > 1 && argv)
-// 	{
-// 		ft_putstr_fd("Program does not accept arguments.\n", 2);
-// 		return (0);
-// 	}
-// 	array_dup(&minishell, envp);
-// 	//testing
-// 	/*minishell.command_list = malloc(sizeof(t_command));
-// 	minishell.command_list->minishell = &minishell;
-// 	minishell.minishell->exit_status = 0;
-// 	init_signals(&minishell);
-// 	while (1)
-// 	{
-// 		command = readline("minishell$ ");
-// 		if (!command)
-// 			return (printf("exit\n"), 1);
-// 		else if (command && command[0])
-// 		{
-// 			add_history(command);
-// 			minishell.command_list->argv = ft_split_argv(command);
-// 			//make_test_nodes(&minishell.ast);
-// 			if (minishell.command_list->argv)
-// 			{
-// 				int	i = 0;
-// 				while (minishell.command_list->argv[i])
-// 				{
-// 					printf("command[%i]: %s\n", i, minishell.command_list->argv[i]);
-// 					i++;
-// 				}
-// 				//begin_execution(&minishell, minishell.ast);
-// 				if (!check_builtin(&minishell, minishell.command_list))
-// 					printf("command is not a builtin\n");
-// 			}
-// 		}
-// 	}*/
-// 	return (0);
-// }
+int	main(int argc, char **argv, char **envp)
+{
+	t_minishell	minishell;
+	char	*command;
+
+	if (argc > 1 && argv)
+	{
+		ft_putstr_fd("Program does not accept arguments.\n", 2);
+		return (0);
+	}
+	array_dup(&minishell, envp);
+	minishell.env_path = get_env_path(envp);
+	//testing
+	minishell.exit_status = 0;
+	init_signals(&minishell);
+	while (1)
+	{
+		command = readline("Minishell:~$ ");
+		if (!command)
+			return (printf("exit\n"), 1);
+		else if (command && command[0])
+		{
+			add_history(command);
+			minishell.tokens = ft_lexer(command);
+			minishell.curr_token = minishell.tokens;
+			ft_parser(&minishell);
+			ft_exec(&minishell);
+		}
+	}
+	return (0);
+}
