561d7039505d60ffcdf0aea8b8bb50c3835a21b8
Avery Gan <agan@c2r4s10.42singapore.sg>
2024-02-09 14:52:46 +0800

    fixed compilation error for error handling

--- srcs/error/print_error.c
+++ srcs/error/print_error.c
@@ -24,7 +24,7 @@ int	print_char_err(int error, char c, t_minishell *minishell)
 	return (set_exit_error(minishell, error, EXIT_FAILURE));
 }
 
-void print_err_helper(int err)
+void print_err_helper(int error, char *str)
 {
 	if (error == OPEN_ERR)
 	{
@@ -37,26 +37,28 @@ void print_err_helper(int err)
 		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": ", 2);
 		ft_putstr_fd("Permission denied\n", 2);
-	}	
+	}
+	else if (error == SYNTAX_ERR)
+	{
+		ft_putstr_fd("syntax error near unexpected token `", 2);
+		ft_putstr_fd(str, 2);
+		ft_putstr_fd("'\n", 2);
+	}
 }
 
 int	print_str_err(int error, char *str, t_minishell *minishell)
 {
 	ft_putstr_fd("minishell: ", 2);
 	if (error == SYNTAX_ERR)
-	{
-		ft_putstr_fd("syntax error near unexpected token `", 2);
-		ft_putstr_fd(str, 2);
-		ft_putstr_fd("'\n", 2);
-	}
+		print_err_helper(SYNTAX_ERR, str);
 	else if (error == DUP2_ERR)
 		ft_putstr_fd("error: dup2 failed\n", 2);
 	else if (error == MEM_ERR)
 		ft_putstr_fd("error: memory allocation failed\n", 2);
 	else if (error == OPEN_ERR)
-		print_err_helper(OPEN_ERR);
+		print_err_helper(OPEN_ERR, str);
 	else if (error == PERM_ERR)
-		print_err_helper(PERM_ERR);
+		print_err_helper(PERM_ERR, str);
 	else if (error == PIPE_ERR || error == FORK_ERR)
 		ft_putstr_fd(str, 2);
 	return (set_exit_error(minishell, error, EXIT_FAILURE));
