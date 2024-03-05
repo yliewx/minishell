dc54076278c7d016590c3f693bd2ffe538a65759
Yun Xuan Liew <yliew@c1r7s3.42singapore.sg>
2024-03-05 17:51:44 +0800

    fixed wildcard pattern matching for multiple of the same character, eg. *s*s*

--- srcs/expander/pattern.c
+++ srcs/expander/pattern.c
@@ -27,44 +27,57 @@ bool	match_visibility(char *pattern, char *name)
 matches the pattern
 - If the pattern does not end with *, check that the name ends with
 the same character */
-bool	match_pattern_suffix(char *curr_pattern, char *entry, int j)
+bool	match_pattern_suffix(char *c_pattern, char *entry, int j)
 {
-	if (curr_pattern[j] != '*'
-		&& (entry[ft_strlen(entry) - 1] != curr_pattern[j]))
+	if (c_pattern[j] != '*'
+		&& (entry[ft_strlen(entry) - 1] != c_pattern[j]))
+		return (false);
+	return (true);
+}
+
+
+/* Function to check if the first character in the entry name
+matches the pattern
+- If the pattern does not begin with a *, check whether the name
+starts with the same character as the pattern */
+bool	match_pattern_prefix(t_node_arg *node_arg, char *c_pattern, char *entry)
+{
+	if (node_arg->pattern == c_pattern && *node_arg->pattern != '*'
+		&& *node_arg->pattern != *entry)
 		return (false);
 	return (true);
 }
 
 /* Function to check if the file/directory name matches the pattern
-- If the pattern does not begin with a *, check whether the name
-starts with the same character as the pattern
+- When reaching the end of the pattern and entry points to null after last char,
+return true
 - If the end of the pattern has been reached, check if the last character
 in the pattern matches the last character in the name */
-bool	match_pattern(t_node_arg *node_arg, char *curr_pattern, char *entry)
+bool	match_pattern(t_node_arg *node_arg, char *c_pattern, char *entry)
 {
 	int	i;
 	int	j;
 
-	if (node_arg->pattern == curr_pattern && *node_arg->pattern != '*'
-		&& *node_arg->pattern != *entry)
+	if (!match_pattern_prefix(node_arg, c_pattern, entry))
 		return (false);
-	while (curr_pattern && *curr_pattern == '*')
-		curr_pattern++;
-	i = 0;
-	while (entry[i])
+	else if (c_pattern && *c_pattern == '*' && !(*entry) && !*(c_pattern + 1))
+		return (true);
+	while (c_pattern && *c_pattern == '*')
+		c_pattern++;
+	i = -1;
+	while (entry[++i])
 	{
 		j = 0;
-		while (curr_pattern[j] && curr_pattern[j] != '*')
+		while (c_pattern[j] && c_pattern[j] != '*')
 		{
-			if (entry[i + j] != curr_pattern[j])
+			if (entry[i + j] != c_pattern[j])
 				break ;
 			j++;
 		}
-		if (!curr_pattern[j])
-			return (match_pattern_suffix(curr_pattern, entry, j - 1));
-		else if (curr_pattern[j] == '*')
-			return (match_pattern(node_arg, curr_pattern + j, entry + i));
-		i++;
+		if (!c_pattern[j])
+			return (match_pattern_suffix(c_pattern, entry, j - 1));
+		else if (c_pattern[j] == '*')
+			return (match_pattern(node_arg, c_pattern + j, entry + i + 1));
 	}
 	return (false);
 }
